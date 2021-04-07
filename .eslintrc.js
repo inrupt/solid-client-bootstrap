66e7b5d17ba634714f92cf1b05cc7f689d5ac34c
Nicolas Seydoux <nseydoux@inrupt.com>
2021-04-07 07:55:03 +0200

    Fixed linting configuration
    @inrupt/eslint-config-react (as opposed to
    @inrupt/eslint-config-react-ts) throws an error when using `import ...
    from "./local-fie`.
    
    This also fixes misc. issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,39 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 module.exports = {
-  extends: ["@inrupt/eslint-config-react"],
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "@inrupt/eslint-config-base",
+    "@inrupt/eslint-config-react-ts",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "jest", "license-header", "import"],
+  rules: {
+    // There's a TypeScript-specific version of this rule;
+    // we disable the generic one, because it thinks imported types are unused
+    // when they're not:
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+    "license-header/header": [
+      process.env.CI ? "error" : "warn",
+      "./resources/license-header.js",
+    ],
+  },
 };
